7548f76968736032c006a49f61feb59cb133c8ab
mpirmoradiyan <mohsenpirmoradian@gmail.com>
2019-10-01 23:09:43 +0200

    commit#1 Writing and designing the code. First Structure Should be modified

---
+++ lab5/R/myMap.R
@@ -0,0 +1,45 @@
+#' @import httr
+#' @import jsonlite
+#' @import ggmap
+
+mapStamen <- setRefClass("mapStamen",
+                         fields = list(
+                           myMap = "raster"
+                           
+                         ),
+                         methods = list(
+                           
+                           initialize = function(left = 86.05,
+                                                 bottom = 27.21,
+                                                 right = 87.81,
+                                                 top = 28.76){
+                             
+                             Prompt <- readline("The default map illustrates
+                             a crop of mountain Everest;Continue with that?[Y/N]: ")
+                             
+                             if(tolower(Prompt) == "n"){
+                               
+                               left <- as.numeric(readline("What is the value of lowerleft lon?"))  
+                               bottom <- as.numeric(readline("What is the value of lowerleft lat?"))
+                               right <- as.numeric(readline("What is the value of upperright lon?"))
+                               top <- as.numeric(readline("What is the value of upperright lat?"))
+                             }
+                             
+                             myBox <- c(left , bottom , right , top )
+                             
+                             
+                             myMap <<- get_stamenmap(bbox = myBox,
+                                           zoom = 10,
+                                           maptype = c("terrain-background"),
+                                           crop = TRUE)
+                           },
+                           
+                           showMap = function(){
+
+                             ggmap(myMap)
+                           }
+                         )
+)
+                         
+
+
