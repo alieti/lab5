32a8b388cb8fb86cbdc71759e04e2ee62524217c
mpirmoradiyan <mohsenpirmoradian@gmail.com>
2019-10-02 12:56:42 +0200

    commit#5 Some changes in code

--- lab5/R/myMap.R
+++ lab5/R/myMap.R
@@ -1,45 +1,60 @@
- library(httr)
- library(jsonlite)
- library(ggmap)
- library(R6)
+library(httr)
+library(jsonlite)
+library(ggmap)
+library(R6)
 
-mapStamen <- R6Class("mapStamen",
-                         public = list(
-                           left = NA,
-                           bottom = NA,
-                           right = NA,
-                           top = NA,
-                           myMap = NA,
-                        
-                           
-                           initialize = function(left, bottom, right, top){
-                             
-                             if (!missing(left)) self$left <- left
-                             if (!missing(bottom)) self$bottom <- bottom
-                             if (!missing(right)) self$right <- right
-                             if (!missing(top)) self$top <- top
-                             
-                             if (missing(left)) {self$left <- as.numeric(readline("What is the value of lowerleft longitude?"))}
-                             if (missing(bottom)) {self$bottom <- as.numeric(readline("What is the value of lowerleft latitude?"))}
-                             if (missing(right)) {self$right <- as.numeric(readline("What is the value of upperright longitude?"))}
-                             if (missing(top)) {self$top <- as.numeric(readline("What is the value of upperright latitude?"))}
-                             
-                             
-                            
-                             self$myMap <- get_stamenmap(c(self$left , self$bottom , self$right , self$top ),
+MapStamen <- R6Class("MapStamen",
+                     public = list(
+                       
+                       initialize = function(left, bottom, right, top){
+                         
+                         # if (missing(left)) {
+                         #left <- as.numeric(readline("What is the value of lowerleft longitude?"))}
+                         # if (missing(bottom)) {
+                         #bottom <- as.numeric(readline("What is the value of lowerleft latitude?"))}
+                         # if (missing(right)) {
+                         #right <- as.numeric(readline("What is the value of upperright longitude?"))}
+                         # if (missing(top)) {
+                         #top <- as.numeric(readline("What is the value of upperright latitude?"))}
+                         
+                         
+                         stopifnot(is.numeric(left), length(left) == 1)
+                         stopifnot(is.numeric(bottom), length(bottom) == 1)
+                         stopifnot(is.numeric(right), length(right) == 1)
+                         stopifnot(is.numeric(top), length(top) == 1)
+                         
+                         private$.left <- left
+                         private$.bottom <- bottom
+                         private$.right <- right
+                         private$.top <- top
+                         
+                         
+                         private$getMap()
+                         
+                       },
+                       
+                       showMap = function(){
+                         
+                         ggmap(private$.myMap)
+                       }
+                     ),
+                     
+                     private = list(
+                       .left = NA,
+                       .bottom = NA,
+                       .right = NA,
+                       .top = NA,
+                       .myMap = NA,
+                       
+                       getMap = function(){
+                         private$.myMap <- get_stamenmap(c(private$.left, private$.bottom,
+                                                           private$.right , private$.top ),
                                                          zoom = 10,
                                                          maptype = c("terrain-background"),
                                                          crop = TRUE)
-                           },
-                           
-                           
-                           
-                           showMap = function(){
-
-                             ggmap(self$myMap)
-                           }
-                         )
-)
                          
-
-
+                         self
+                       }
+                       
+                     )
+)
